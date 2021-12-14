963db07cf89088861ba4bd0bea8f5c216a832f9b
MateuszJ <mateuszjonczak@gmail.com>
2021-12-14 16:42:00 +0100

    added hex library

--- client/src/main/java/com/client/Window.java
+++ client/src/main/java/com/client/Window.java
@@ -4,10 +4,16 @@ import com.client.helpers.Routes;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.hexworks.mixite.core.api.*;
+import org.hexworks.mixite.core.api.defaults.DefaultSatelliteData;
 
 public class Window extends Application {
 
@@ -29,10 +35,24 @@ public class Window extends Application {
         Image icon = new Image(Routes.imageRoute("pawn.png"));
         // imageRoute();
 
+
+        HexagonalGridBuilder<DefaultSatelliteData> builder = new HexagonalGridBuilder<DefaultSatelliteData>()
+                .setGridHeight(11)
+                .setGridWidth(11)
+                .setGridLayout(HexagonalGridLayout.HEXAGONAL)
+                .setOrientation(HexagonOrientation.FLAT_TOP)
+                .setRadius(30);
+        Group group = new Group();
+        Scene scene1 = new Scene(group);
+        HexagonalGrid<DefaultSatelliteData> grid = builder.build();
+        for (Hexagon<DefaultSatelliteData> hex: grid.getHexagons()) {
+            group.getChildren().add(new Circle(hex.getCenterX(),hex.getCenterY(), 20, Color.VIOLET));
+            group.getChildren().add(new Text(hex.getCenterX(),hex.getCenterY(),hex.getGridX() + "," + hex.getGridY() + "," + hex.getGridZ()));
+        }
         // Stage settings
         primaryStage.getIcons().add(icon);
         primaryStage.setTitle("WarcabyGame");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(scene1);
         primaryStage.show();
         primaryStage.setWidth(1200);
         primaryStage.setHeight(800);
