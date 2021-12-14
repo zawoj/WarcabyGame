e69c6236b1353e50d2034731894977fccd458fce
Kacper Zawojski <zawojweb@gmail.com>
2021-12-14 14:05:27 +0100

    Create Routes
    Create routes for:
    - Images
    - CSS styles
    - Views in fxml

--- client/src/main/java/com/client/helpers/Routes.java
+++ client/src/main/java/com/client/helpers/Routes.java
@@ -1,14 +1,29 @@
 package com.client.helpers;
 
 import java.io.File;
-
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Routes {
-    
-    public String imageRoute(String imgName){
-        File file = new File("G:\\WarcabyGame\\client\\src\\img\\",imgName);
+
+    public String imageRoute(String imgName) {
+        File file = new File("G:\\WarcabyGame\\client\\src\\main\\java\\com\\client\\img", imgName);
 
         return file.toURI().toString();
-        
+
+    }
+
+    public String styleRoute(String styleFileName) {
+        File file = new File("G:\\WarcabyGame\\client\\src\\main\\java\\com\\client\\styles", styleFileName);
+
+        return file.toURI().toString();
+
+    }
+
+    public URL viewsRoute(String viewName) throws MalformedURLException {
+        File file = new File("G:\\WarcabyGame\\client\\src\\main\\java\\com\\client\\views", viewName);
+
+        return file.toURI().toURL();
+
     }
 }
