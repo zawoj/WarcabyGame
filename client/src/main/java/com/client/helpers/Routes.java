f1947caee5c6dcc59385a0f19e732d9ea1629fae
MateuszJ <mateuszjonczak@gmail.com>
2021-12-14 16:31:55 +0100

    changes to Routes

--- client/src/main/java/com/client/helpers/Routes.java
+++ client/src/main/java/com/client/helpers/Routes.java
@@ -7,21 +7,21 @@ import java.net.URL;
 public class Routes {
 
     public static String imageRoute(String imgName) {
-        File file = new File(System.getProperty("user.dir"), "client\\src\\main\\java\\com\\client\\img\\" + imgName);
+        File file = new File( "src\\main\\java\\com\\client\\img\\" + imgName);
 
         return file.toURI().toString();
 
     }
 
     public static String styleRoute(String styleFileName) {
-        File file = new File("G:\\WarcabyGame\\client\\src\\main\\java\\com\\client\\styles", styleFileName);
+        File file = new File("src\\main\\java\\com\\client\\styles" + styleFileName);
 
         return file.toURI().toString();
 
     }
 
     public static URL viewsRoute(String viewName) throws MalformedURLException {
-        File file = new File(System.getProperty("user.dir"), "client\\src\\main\\java\\com\\client\\views\\" + viewName);
+        File file = new File("src\\main\\java\\com\\client\\views\\" + viewName);
 
         return file.toURI().toURL();
 
