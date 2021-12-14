cda7079a57096180db73e2d7fdddf474b15b7a39
Kacper Zawojski <zawojweb@gmail.com>
2021-12-14 14:55:36 +0100

    Update App.java

--- client/src/main/java/com/client/App.java
+++ client/src/main/java/com/client/App.java
@@ -4,13 +4,9 @@ import com.client.helpers.Routes;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -28,26 +24,12 @@ public class App extends Application {
         // FXML loader
         Parent sceneRoot = FXMLLoader.load(route.viewsRoute("testView.fxml"));
 
-        // Create Group node which will be the root
-        Group root = new Group();
-
         // Basic scene
         Scene scene = new Scene(sceneRoot, 500, 500);
-
-        // Text
-        Text text = new Text();
-        text.setText("LogIn");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("Verdana", 50));
-        text.setFill(Color.GOLD);
 
         // Icone
         Image icon = new Image(route.imageRoute("pawn.png"));
         // imageRoute();
-
-        // Settings for root
-        root.getChildren().add(text);
 
         // Stage settings
         primaryStage.getIcons().add(icon);
@@ -57,11 +39,6 @@ public class App extends Application {
         primaryStage.setWidth(1200);
         primaryStage.setHeight(800);
         primaryStage.centerOnScreen();
-        // primaryStage.setX(50);
-        // primaryStage.setY(50);
-        // primaryStage.setFullScreen(true);
-        // primaryStage.setFullScreenExitHint("press q");
-        // primaryStage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
 
     }
 
