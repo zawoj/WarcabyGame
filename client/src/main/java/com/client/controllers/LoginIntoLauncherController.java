b5ecd6bccc659acc120cfa75e8fe526691401fc9
Kacper Zawojski <zawojweb@gmail.com>
2021-12-19 21:54:27 +0100

    Login controller and simple validation

--- client/src/main/java/com/client/controllers/LoginIntoLauncherController.java
+++ client/src/main/java/com/client/controllers/LoginIntoLauncherController.java
@@ -1,16 +1,22 @@
 package com.client.controllers;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.client.ClientCore;
 import com.client.helpers.Routes;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class LoginIntoLauncherController {
@@ -18,48 +24,68 @@ public class LoginIntoLauncherController {
     @FXML
     private Button LoginButton, RegisteryButton;
     @FXML
-    private TextField NickNameTextField;
+    private TextField LoginField, PasswordField;
     @FXML
     private URL location;
+    @FXML
+    private Pane ErrorPane;
+
+    private static Stage stage;
+    private static Parent root;
 
     @FXML
     private void LoginLauncherControllerButtons(ActionEvent event) throws Exception {
-        // TODO implement validation
-        Stage stage;
-        Parent root;
+        TranslateTransition transition = new TranslateTransition();
+        transition.setNode(ErrorPane);
 
-        // TODO clean code
-        if (event.getSource() == LoginButton) {
-            stage = (Stage) LoginButton.getScene().getWindow();
-            FXMLLoader loader = new FXMLLoader(Routes.viewsRoute("DashboardView.fxml"));
-            root = loader.load();
-            Scene scene = new Scene(root, 1200, 800);
-
-            DashboardController dashboardController = loader.getController();
-            dashboardController.displayNickName(NickNameTextField.getText());
-
-            scene.getStylesheets().add(Routes.styleRoute("app.css"));
-            stage.setScene(scene);
-            stage.show();
-
-        } else if (event.getSource() == RegisteryButton) {
-            System.out.println("Registery");
-            stage = (Stage) RegisteryButton.getScene().getWindow();
-            root = FXMLLoader.load(Routes.viewsRoute("RegisterView.fxml"));
-            Scene scene = new Scene(root, 600, 400);
-            scene.getStylesheets().add(Routes.styleRoute("app.css"));
-            stage.setScene(scene);
-            stage.show();
-
-        } else {
-
+        try {
+            if (event.getSource() == LoginButton) {
+                System.out.println(LoginField.getText() + PasswordField.getText());
+                ClientCore.getInstance().reqLogin(LoginField.getText(), PasswordField.getText());
+                LoadDashboardScene();
+            } else if (event.getSource() == RegisteryButton) {
+                LoadRegisteryScene();
+            }
+        } catch (Exception e) {
+            transition.setToX(-285);
+            transition.play();
         }
 
     }
 
     @FXML
+    private void errorButton(ActionEvent e) {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setNode(ErrorPane);
+        transition.setToX(0);
+        transition.play();
+    }
+
+    @FXML
     void initialize() {
 
+    }
+
+    private void LoadDashboardScene() throws MalformedURLException, IOException {
+        stage = (Stage) LoginButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Routes.viewsRoute("DashboardView.fxml"));
+        root = loader.load();
+        Scene scene = new Scene(root, 1200, 800);
+
+        DashboardController dashboardController = loader.getController();
+        dashboardController.displayNickName(LoginField.getText());
+        scene.getStylesheets().add(Routes.styleRoute("app.css"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void LoadRegisteryScene() throws MalformedURLException, IOException {
+        stage = (Stage) RegisteryButton.getScene().getWindow();
+        root = FXMLLoader.load(Routes.viewsRoute("RegisterView.fxml"));
+        Scene scene = new Scene(root, 600, 400);
+        scene.getStylesheets().add(Routes.styleRoute("app.css"));
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- client/src/main/java/com/client/controllers/StartViewController.java
+++ client/src/main/java/com/client/controllers/StartViewController.java
@@ -63,7 +63,7 @@ public class StartViewController implements Initializable {
         stage = (Stage) connectServerButton.getScene().getWindow();
         root = FXMLLoader.load(Routes.viewsRoute("loginIntoLauncher.fxml"));
         System.out.println("Connect Server");
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root, 800, 600);
         scene.getStylesheets().add(Routes.styleRoute("app.css"));
 
         stage.setScene(scene);
