38cbf693734587b440806050eacb7268ca953d36
Kacper Zawojski <zawojweb@gmail.com>
2021-12-19 21:53:33 +0100

    Add login client core method

--- client/src/main/java/com/client/ClientCore.java
+++ client/src/main/java/com/client/ClientCore.java
@@ -1,9 +1,5 @@
 package com.client;
 
-import com.client.controllers.StartViewController;
-
-import java.net.Socket;
-
 /**
  * class being the core of the client.
  * this class is a singleton
@@ -12,11 +8,10 @@ import java.net.Socket;
 public class ClientCore {
     private static volatile ClientCore instance;
 
-
-    public static ClientCore getInstance(){
-        if(instance == null){
-            synchronized (ClientCore.class){
-                if(instance == null){
+    public static ClientCore getInstance() {
+        if (instance == null) {
+            synchronized (ClientCore.class) {
+                if (instance == null) {
                     instance = new ClientCore();
                 }
             }
@@ -24,9 +19,24 @@ public class ClientCore {
         return instance;
     }
 
-    public void reqServerConnection(String ip, String port) throws Exception{
-        Socket client = new Socket(ip,Integer.parseInt(port));
-        ConnectionListener cl = new ConnectionListener(client);
-        cl.start();
+    public void reqServerConnection(String ip, String port) throws Exception {
+
+        // TODO send really req to server now is just for testing client
+        if (ip.equals("100.00") && port.equals("8080")) {
+            System.out.println("connection established");
+
+        } else {
+            throw new Exception("cannot connect");
+        }
+    }
+
+    public void reqLogin(String login, String password) throws Exception {
+        System.out.println(login + password);
+        // TODO send really req to server now is just for testing client
+        if (login.equals("Zawoj") && password.equals("1234")) {
+            System.out.println("Loged successfully");
+        } else {
+            throw new Exception("Can't Log in");
+        }
     }
 }
