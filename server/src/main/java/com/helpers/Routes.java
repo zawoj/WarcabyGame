9991082dd43bbf4415c2321acb4db2da4ece4a02
Kacper Zawojski <zawojweb@gmail.com>
2021-12-20 23:12:43 +0100

    adding json db succesfully

--- server/src/main/java/com/helpers/Routes.java
+++ server/src/main/java/com/helpers/Routes.java
@@ -13,8 +13,11 @@ public class Routes {
 
     public static String styleRoute(String styleFileName) throws MalformedURLException {
         File file = new File("src\\main\\java\\com\\styles\\" + styleFileName);
-
         return file.toURI().toString();
+    }
 
+    public static File databaseRoute(String dbFileName) throws MalformedURLException {
+        File file = new File("DB\\" + dbFileName);
+        return file;
     }
 }
