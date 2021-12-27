ea7ab375d0e98cce48afff6e380681f4b8552351
Kacper Zawojski <zawojweb@gmail.com>
2021-12-27 15:45:56 +0100

    Add edit game name

--- client/src/main/java/com/client/controllers/LobbyController.java
+++ client/src/main/java/com/client/controllers/LobbyController.java
@@ -4,7 +4,9 @@ import com.client.ClientCore;
 import com.client.Lobby;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
@@ -15,14 +17,35 @@ public class LobbyController {
     @FXML
     private ImageView avatarImage;
     @FXML
-    Label gameName;
+    TextField gameName;
+    @FXML
+    Button saveEditButton;
+
+    private Boolean saveMode = false;
 
     @FXML
     public void initialize() {
         displayNickName(ClientCore.getInstance().getLogin());
         Lobby newLobby = new Lobby(ClientCore.getInstance().getLogin());
-        gameName.setText("Game Name");
+        gameName.setPromptText("Host's game");
+        gameName.setDisable(true);
 
+    }
+
+    @FXML
+    public void saveEdit() {
+        saveMode = !saveMode;
+        System.out.println(saveMode);
+        if (saveMode == true) {
+            // After edit
+            saveEditButton.setText("Save");
+            gameName.setDisable(false);
+        } else {
+            // After Save
+            saveEditButton.setText("Edit");
+            gameName.setDisable(true);
+            System.out.println(gameName.getText());
+        }
     }
 
     public void displayNickName(String nickName) {
