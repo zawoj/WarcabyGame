6f1274f6378cb40af58abc59b091d8dca4235d42
Kacper Zawojski <zawojweb@gmail.com>
2022-01-08 14:24:43 +0100

    Configure TestFX

--- client/src/test/java/com/client/controllerTest/RegisteryControllerTest.java
+++ client/src/test/java/com/client/controllerTest/RegisteryControllerTest.java
@@ -10,7 +10,10 @@ import com.client.helpers.exceptions.StringSameValidation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
 
+@ExtendWith(ApplicationExtension.class)
 public class RegisteryControllerTest {
     RegisteryController instance;
 
