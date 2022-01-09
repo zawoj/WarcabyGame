13cb7925f1574396983887ba0cb8810002212646
Kacper Zawojski <zawojweb@gmail.com>
2022-01-10 00:11:53 +0100

    Add test for ClientCore

--- client/src/main/java/com/client/ConnectionListener.java
+++ client/src/main/java/com/client/ConnectionListener.java
@@ -83,7 +83,6 @@ public class ConnectionListener extends Thread {
                 if (Objects.equals(jlm.getHostName(), ClientCore.getInstance().getLogin())) {
                     ClientCore.getInstance().myTurn = true;
                 }
-                // TODO This broke when is next turn
                 ClientCore.getInstance().currentPlayer = jlm.getHostName();
                 ClientCore.getInstance().getGameController().setTurnArrow();
 
